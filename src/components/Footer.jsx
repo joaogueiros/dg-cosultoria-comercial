74e28fffd13e554a8c20639710470e1e2ba2b23e
João Gueiros <joao.gueiros@192.168.0.19>
2026-07-06 18:55:20 -0300

    Adicionando linhas para carregar o vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,6 +50,7 @@ export default function Footer() {
               >
                 WhatsApp comercial
               </a>
+              
             </li>
             <li>
               <a href="mailto:consultoriacomercial.dg@gmail.com">
--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -16,5 +16,7 @@ export default function WhatsappButton() {
         />
       </svg>
     </a>
+
+    
   )
 }
